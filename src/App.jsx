3728733bb7819141b0a1f7c71db6706abda2cf65
FridaEriksson95 <fridaeriiksson@gmail.com>
2025-09-18 11:20:06 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import GetApi from './Components/GetApi'
+
+function App() {
+
+  return (
+    <>
+     <GetApi />
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/GetApi.jsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+
+const GetApi = () => {
+    const [buttonTrigger, setButtonTrigger] = useState(0);
+    const [gif, setGif] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(() => {
+        const fetchRandomGif = async () => {
+            setIsLoading(true);
+            setGif(null);
+            const randomUrl = 'https://api.giphy.com/v1/gifs/random';
+            const apiKey = 'zMyMJNyyQKdnS6TQwVRYxGDd2piLMC2F';
+            const url = `${randomUrl}?api_key=${apiKey}`;
+
+            try{
+            const response = await fetch(url);
+            const gifData = await response.json();
+            setGif(gifData.data);
+            } catch (error){
+                setGif(null);
+            } finally {
+                setIsLoading(false);
+            }
+        };
+
+        if(buttonTrigger > 0){
+            fetchRandomGif();
+        }
+    }, [buttonTrigger]);
+
+    const handleClick = () => {
+        setButtonTrigger((prev) => prev + 1);
+    }
+
+    return(
+        <>
+        <button onClick={handleClick} disabled={isLoading}>Klicka för Random GIF</button>
+        <br />
+        {!isLoading && gif && (
+            <img src={gif.images.fixed_width.url} alt={gif.title} style={{maxWidth: '350px', marginTop: '20px'}}
+            /> )}
+        </>
+    )
+}
+
+export default GetApi;
